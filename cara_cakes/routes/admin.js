c5afc38036be707b696d827d222760f7e4ee1b24
Jume Brice <bricejume@gmail.com>
2020-03-23 15:21:34 +0000

    Improving app validation

--- cara_cakes/routes/admin.js
+++ cara_cakes/routes/admin.js
@@ -2,6 +2,7 @@
 const getAdminRoutes = require('../controllers/admin');
 const express = require('express');
 const router = express.Router();
+const { check, body } = require('express-validator');
 
 router.get('/', getAdminRoutes.getIndex);
 router.get('/general', getAdminRoutes.getGeneral);
@@ -40,9 +41,31 @@ router.get('/orders', getAdminRoutes.getOrders);
 
 router.get('/orders/:orderId', getAdminRoutes.getClientOrder);
 
-router.post('/add-pastry', getAdminRoutes.postAddPastry);
+router.post('/add-pastry', [
+    body('name')
+    .isAlphanumeric()
+    .isLength({
+        min: 5
+    })
+    .trim(),
+    body('price')
+    .isNumeric(),
+    body('image').isLength({ min: 4}),
+    body('desc').isLength({ min: 10, max: 1000 }).trim()
+], getAdminRoutes.postAddPastry);
 
-router.post('/edit-pastry', getAdminRoutes.postEditPastry);
+router.post('/edit-pastry', [
+    body('name')
+    .isAlphanumeric()
+    .isLength({
+        min: 5
+    })
+    .trim(),
+    body('price')
+    .isNumeric(),
+    body('image').isLength({ min: 4}),
+    body('desc').isLength({ min: 10, max: 1000 }).trim()
+], getAdminRoutes.postEditPastry);
 
 router.get('/edit-pastry/:pastryId', getAdminRoutes.getEditPastry);
 
